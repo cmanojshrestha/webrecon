41a50f4e9bd370a78839d9f8e7ce4400121f74e3
manojxshrestha <106522935+manojxshrestha@users.noreply.github.com>
2023-02-08 14:21:41 +0545

    Update subenum.pl

--- subenum.pl
+++ subenum.pl
@@ -1,98 +1,99 @@
 #!/bin/bash
 
+# Add your name
+Author="Manoj Shrestha"
+
+# Check if domain name is provided as command line argument
+if [ $# -eq 0 ]; then
+  echo "Please provide a domain name as argument"
+  echo "Example: $0 example.com"
+  exit 1
+fi
+
 domain=$1
 
-# Discover subdomains using amass
+# Check if amass is installed
+if ! command -v amass &> /dev/null
+then
+  echo "Amass is not installed, please install it"
+  exit 1
+fi
 
+# Check if httpx is installed
+if ! command -v httpx &> /dev/null
+then
+  echo "Httpx is not installed, please install it"
+  exit 1
+fi
+
+# Check if eyewitness is installed
+if ! command -v eyewitness &> /dev/null
+then
+  echo "Eyewitness is not installed, please install it"
+  exit 1
+fi
+
+# Check if masscan is installed
+if ! command -v masscan &> /dev/null
+then
+  echo "Masscan is not installed, please install it"
+  exit 1
+fi
+
+# Check if dirbust is installed
+if ! command -v dirbust &> /dev/null
+then
+  echo "Dirbust is not installed, please install it"
+  exit 1
+fi
+
+# Check if gitleaks is installed
+if ! command -v gitleaks &> /dev/null
+then
+  echo "Gitleaks is not installed, please install it"
+  exit 1
+fi
+
+# Check if gau is installed
+if ! command -v gau &> /dev/null
+then
+  echo "Gau is not installed, please install it"
+  exit 1
+fi
+
+# Check if jminer is installed
+if ! command -v jminer &> /dev/null
+then
+  echo "Jminer is not installed, please install it"
+  exit 1
+fi
+
+echo "Discovering subdomains using amass..."
 amass enum -d $domain -o subdomains.txt
 
-# Check subdomains for live or dead
-
+echo "Checking subdomains for live or dead..."
 cat subdomains.txt | while read sub; do
-
   httpx $sub | grep -q "200 OK"
-
   if [ $? -eq 0 ]; then
-
     echo "$sub is live" >> live_subdomains.txt
-
   else
-
     echo "$sub is dead" >> dead_subdomains.txt
-
   fi
-
 done
 
-# Take screenshots of live subdomains
-
+echo "Taking screenshots of live subdomains..."
 while read sub; do
-
   eyewitness --headless -f live_subdomains.txt --threads 100 --prepend-https
-
 done < live_subdomains.txt
 
-# Portscan top ports using masscan
-
+echo "Portscanning top ports using masscan..."
 masscan -p1-10000,80,443 $(cat live_subdomains.txt | sed 's/^/--adapter-port /') > ports.txt
 
-# Dirbuster with common wordlist
-
+echo "Dirbusting with common wordlist..."
 dirbusting_wordlist="dirbusting_wordlist.txt"
-
 while read sub; do
-
   dirbust --wordlist=$dirbusting_wordlist $sub
-
 done < live_subdomains.txt
 
-# Gitleaks search for leaks
-
-gitleaks --repo-path=$domain --verbose
-
-# Extract JavaScript files and search for API keys
-
-cat live_subdomains.txt | while read sub; do
-
-  curl -L $sub | grep -Eo "(http|https)://[a-zA-Z0-9./?=_-]*\.js" | while read js; do
-
-    curl -L $js > "$sub.js"
-
-    jminer "$sub.js"
-
-  done
-
-done
-
-# Search in web archive
-
-gau $domain > archive.txt
-
-# Create HTML report
-
-echo "<html><body><h1>Results for $domain</h1><h2>Live Subdomains</h2><ul>" >> report.html
-
-while read sub; do
-
-  echo "<li>$sub</li>" >> report.html
-
-done < live_subdomains.txt
-
-echo "</ul><h2>Dead Subdomains</h2><ul>" >> report.html
-
-while read sub; do
-
-  echo "<li>$sub</li>" >> report.html
-
-done < dead_subdomains.txt
-
-echo "</ul><h2>Ports</h2><pre>" >> report.html
-
-cat ports.txt >> report.html
-
-echo "</pre><h2>Web Archive</h2><pre>" >> report.html
-
-cat archive.txt >> report.html
-
-echo "</pre></body></html>" >> report.html
-
+echo "Searching for leaks using gitleaks..."
+gitleaks --repo-path=$
